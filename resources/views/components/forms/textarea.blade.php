44e97caa89a210fe7689b7d51cc63959f68a3cfb
Eric Woods <woodseowl@gmail.com>
2024-12-10 15:42:25 -0500

    Adding Flux to scope forms
    Signed-off-by: Eric Woods <woodseowl@gmail.com>

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -1,8 +1,28 @@
 @props([
     'label',
     'id' => $attributes->whereStartsWith('wire:model')->first(),
+    'toolbar' => 'heading bold italic underline | bullet ordered blockquote | link code ~ undo redo',
 ])
-<x-cd.form.form-item field="{{ $id }}">
-    <x-slot name="field_title">{{ $label }}</x-slot>
-    <textarea {{ $attributes->merge(['id' => $id]) }}>{{ $slot }}</textarea>
-</x-cd.form.form-item>
+<flux:editor :$attributes class="max-w-[600px] border-cds-gray-400 border-b-gray-400">
+    <flux:editor.toolbar class="border-b-cds-gray-200">
+        <flux:editor.heading />
+        <flux:editor.separator />
+        <flux:editor.bold />
+        <flux:editor.italic />
+        <flux:editor.strike />
+        <flux:editor.separator />
+        <flux:editor.bullet />
+        <flux:editor.ordered />
+        <flux:editor.blockquote />
+        <flux:editor.separator />
+        <flux:editor.link />
+        <flux:editor.code />
+
+        <flux:editor.spacer />
+
+        <flux:editor.undo />
+        <flux:editor.redo />
+    </flux:editor.toolbar>
+
+    <flux:editor.content />
+</flux:editor>
