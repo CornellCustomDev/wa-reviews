78921d67725e1ad107d269651a4a5e4a01a7bae2
Eric Woods <woodseowl@gmail.com>
2024-12-12 11:19:53 -0500

    Layout cleanup
    Signed-off-by: Eric Woods <woodseowl@gmail.com>

--- resources/views/components/cd/layout/breadcrumbs.blade.php
+++ resources/views/components/cd/layout/breadcrumbs.blade.php
@@ -1,7 +1,7 @@
 @props(['breadcrumbs' => []])
 <div id="breadcrumb-navigation" {{ $attributes }}>
     <nav class="breadcrumb" aria-label="Breadcrumb">
-        <flux:breadcrumbs>
+        <flux:breadcrumbs class="mb-4">
             <flux:breadcrumbs.item :href="route('welcome')" class="text-xs" icon="home" />
             @foreach($breadcrumbs as $breadcrumb => $route)
                 <flux:breadcrumbs.item :href="$route">{{ $breadcrumb }}</flux:breadcrumbs.item>
--- resources/views/components/cd/layout/site-header.blade.php
+++ resources/views/components/cd/layout/site-header.blade.php
@@ -2,7 +2,7 @@
     <nav class="dropdown-menu dropdown-menu-on-demand" id="main-navigation" aria-label="Main Navigation">
         <div class="container-fluid">
             <a id="mobile-home" href="#"><span class="sr-only">Home</span></a>
-            <ul class="list-menu links">
+            <ul class="list-menu links [&_li]:!border-r-0">
                 <li><a href="{{ route('projects') }}">Projects</a></li>
                 <li><a href="{{ route('guidelines.index') }}">Guidelines</a></li>
                 <li><a href="{{ route('categories.index') }}">Categories</a></li>
