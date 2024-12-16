3f7b1e09914837f8a84e856d883e2f7f284858ac
Eric Woods <woodseowl@gmail.com>
2024-12-16 14:12:13 -0500

    Fix flux editor borders

--- resources/views/flux/editor/index.blade.php
+++ resources/views/flux/editor/index.blade.php
@@ -1,26 +1,30 @@
 @props([
-    'variant' => null,
     'name' => $attributes->whereStartsWith('wire:model')->first(),
     'toolbar' => null,
     'invalid' => null,
+    'variant' => null,
 ])
 
 @php
 $invalid ??= ($name && $errors->has($name));
 
 $border ??= $invalid ? 'border-red-500' : match ($variant) {
-    'cds' => 'rounded-none border-cds-gray-400 border-b-gray-400',
-    default => 'rounded-lg',
+    default => 'border-zinc-200 border-b-zinc-300/80 dark:border-white/10',
+    'cds' => 'border-cds-gray-400 border-b-gray-400',
 };
 
 $classes = Flux::classes()
     ->add('block w-full')
-    ->add('shadow-sm [&[disabled]]:shadow-none border ' . $border)
+    ->add('shadow-sm [&[disabled]]:shadow-none border')
+    ->add(match ($variant) {
+        default => 'rounded-lg',
+        'cds' => 'rounded-none',
+    })
     ->add('bg-white dark:bg-white/10 dark:[&[disabled]]:bg-white/[7%]')
     ->add('[&_[data-slot=content]]:text-sm')
     ->add('[&_[data-slot=content]]:text-zinc-700 dark:[&_[data-slot=content]]:text-zinc-300')
     ->add('[&[disabled]_[data-slot=content]]:text-zinc-500 dark:[&[disabled]_[data-slot=content]]:text-zinc-400')
-    ->add($invalid ? 'border-red-500' : 'border-zinc-200 border-b-zinc-300/80 dark:border-white/10')
+    ->add($invalid ? 'border-red-500' : $border)
     ;
 @endphp
 
