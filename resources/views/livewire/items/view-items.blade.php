5c847acad60ecc33b0913facbe70368de8ccf638
Eric Woods <woodseowl@gmail.com>
2025-01-14 15:41:00 -0500

    Add image viewer

--- resources/views/livewire/items/view-items.blade.php
+++ resources/views/livewire/items/view-items.blade.php
@@ -25,6 +25,28 @@
                 </td>
                 <td style="vertical-align: top">
                     {!! $item->description !!}
+
+                    @if(!empty($item->image_links))
+                        <div class="flex flex-wrap gap-1 mt-1">
+                            @foreach($item->image_links as $imagePath)
+                                @php($imageName = pathinfo($imagePath, PATHINFO_BASENAME))
+                                <flux:tooltip position="bottom" class="align-middle">
+                                    <flux:button wire:click="viewImage('{{ $imagePath }}')" :loading="false" class="!px-0.5 w-24 h-24 overflow-hidden hover:border-cds-blue-900">
+                                        <div class="relative w-full h-full">
+                                            <img
+                                                src="{{ $imagePath }}"
+                                                alt="Preview of image: {{ $imageName }}"
+                                                class="absolute top-0.5 left-0 object-cover object-top rounded"
+                                            />
+                                        </div>
+                                    </flux:button>
+                                    <flux:tooltip.content>
+                                        View image {{ $imageName }}
+                                    </flux:tooltip.content>
+                                </flux:tooltip>
+                            @endforeach
+                        </div>
+                    @endif
                 </td>
                 <td style="vertical-align: top">
                     {!! $item->recommendation !!}
@@ -54,6 +76,14 @@
     <flux:modal name="edit-item" wire:close="closeEdit()" class="max-w-screen-xl">
         @if ($editItem)
             <livewire:items.update-item :item="$editItem" />
+        @endif
+    </flux:modal>
+    <flux:modal name="view-image" class="max-w-4xl" wire:close="closeImage()">
+        @if ($selectedImage)
+            <flux:subheading class="mb-2">{{ basename($selectedImage) }}</flux:subheading>
+            <div class="border border-cds-gray-900">
+                <img src="{{ $selectedImage }}" alt="Selected Image" class="w-full h-auto">
+            </div>
         @endif
     </flux:modal>
 </div>
