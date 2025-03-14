ddc2510165bb8e6b0e7d28d028e8160b6ceaa9ad
Eric Woods <woodseowl@gmail.com>
2025-03-14 09:27:41 -0400

    Composer require spatie/permission

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -45,4 +45,5 @@ return array(
     '61ba3e92ef91a7a0f1cc260ebd9290e4' => $vendorDir . '/openai-php/client/src/OpenAI.php',
     'ec07570ca5a812141189b1fa81503674' => $vendorDir . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
     '320163ac6b93aebe3dc25b60a0533d56' => $vendorDir . '/spatie/laravel-ignition/src/helpers.php',
+    '377b22b161c09ed6e5152de788ca020a' => $vendorDir . '/spatie/laravel-permission/src/helpers.php',
 );
