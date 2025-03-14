d9aaead84efd6c802e15bd519f7255ea20370f3b
Sean Gnau <sg667@cornell.edu>
2025-03-14 14:27:13 -0400

    Removed redundancy from fieldset.blade.php

--- resources/views/components/forms/fieldset.blade.php
+++ resources/views/components/forms/fieldset.blade.php
@@ -10,18 +10,13 @@
 @if ($description)
 <flux:fieldset legend="{{ $legend }}" aria-describedby="{{ $descr_id }}" class="mb-4 semantic">
     <div class="ml-4 -mr-4">
-        @if ($description)
-            <p id="{{ $descr_id }}">{{ $description }}</p>
-        @endif
+        <p id="{{ $descr_id }}">{{ $description }}</p>
         {{ $slot }}
     </div>
 </flux:fieldset>
 @else
 <flux:fieldset legend="{{ $legend }}" class="mb-4 semantic">
     <div class="ml-4 -mr-4">
-        @if ($description)
-            <p id="{{ $descr_id }}">{{ $description }}</p>
-        @endif
         {{ $slot }}
     </div>
 </flux:fieldset>
