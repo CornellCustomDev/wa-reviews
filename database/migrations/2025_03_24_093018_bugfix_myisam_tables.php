af362ca9f30b3f6b051ad4206595297596dbefd4
Eric Woods <woodseowl@gmail.com>
2025-04-14 09:34:38 -0400

    Bugfix: correct DB table types

---
+++ database/migrations/2025_03_24_093018_bugfix_myisam_tables.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Fix MyISAM tables to InnoDB.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE permissions ENGINE=InnoDB');
+        DB::statement('ALTER TABLE permission_role ENGINE=InnoDB');
+        DB::statement('ALTER TABLE permission_user ENGINE=InnoDB');
+        DB::statement('ALTER TABLE roles ENGINE=InnoDB');
+        DB::statement('ALTER TABLE role_user ENGINE=InnoDB');
+        DB::statement('ALTER TABLE teams ENGINE=InnoDB');
+        DB::statement('ALTER TABLE team_user ENGINE=InnoDB');
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
