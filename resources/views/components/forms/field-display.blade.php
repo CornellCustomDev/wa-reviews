d3036e8356174c189bfbdd9eb208e68422250851
Eric Woods <woodseowl@gmail.com>
2025-05-16 12:55:46 -0400

    Inline field display

--- resources/views/components/forms/field-display.blade.php
+++ resources/views/components/forms/field-display.blade.php
@@ -1,10 +1,20 @@
 @props([
     'label',
+    'variation' => 'default',
 ])
 
 <div data-cds-field-display {{ $attributes }}>
-    <flux:heading level="3">
-        {{ $label }}
-    </flux:heading>
-    <div>{{ $slot }}</div>
+    @if($variation === 'inline')
+        <flux:heading level="3" class="inline-block">
+            {{ $label }}:
+        </flux:heading>
+        <div class="inline-block">
+            {{ $slot }}
+        </div>
+    @else
+        <flux:heading level="3">
+            {{ $label }}
+        </flux:heading>
+        <div>{{ $slot }}</div>
+    @endif
 </div>
