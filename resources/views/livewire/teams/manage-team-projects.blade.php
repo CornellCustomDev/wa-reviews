650bccf92f937e1258589dfaf35f738024e8307f
Eric Woods <woodseowl@gmail.com>
2025-05-23 13:48:18 -0400

    Team display

--- resources/views/livewire/teams/manage-team-projects.blade.php
+++ resources/views/livewire/teams/manage-team-projects.blade.php
@@ -11,7 +11,7 @@
         </tr>
         </thead>
         <tbody>
-        @foreach($this->projects as $project)
+        @foreach($projects as $project)
             <tr wire:key="{{ $project->id }}">
                 <td><a href="{{ route('project.show', $project) }}">{{ $project->name }}</a></td>
                 <td><a href="{{ $project->site_url }}" target="_blank">{{ Str::limit($project->site_url, 40) }}</td>
