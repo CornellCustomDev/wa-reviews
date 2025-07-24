48b6f16f33682ad529ba86c3da517aa3a0341dc4
Eric Woods <woodseowl@gmail.com>
2025-07-24 15:54:24 -0400

    Add UX non-WCAG guideline

---
+++ database/migrations/2025_07_24_145512_add_row_to_guidelines.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('criteria')->insert([
+            'name' => 'Non-WCAG User Experience',
+            'number' => 'UX',
+            'level' => 'UX',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        $id = DB::table('criteria')->where('number', 'UX')->value('id');
+
+        DB::table('guidelines')->insert([
+            'id' => 102,
+            'number' => 102,
+            'name' => 'Non-WCAG user experience issue.',
+            'notes' => '<h3>WCAG 2 criterion</h3><p>Non-WCAG</p><h3>Tools and requirements</h3><h3>Note</h3><p>See observed functionality.</p>',
+            'tools' => '[]',
+            'criterion_id' => $id,
+            'category_id' => 11,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('guidelines')->where('number', '102')->delete();
+        DB::table('criteria')->where('number', 'UX')->delete();
+    }
+
+};
