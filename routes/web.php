7e077adb51a845dbc91f2afc8013544bdc878bd7
Eric Woods <woodseowl@gmail.com>
2025-08-03 11:56:37 -0400

    Wrap app in AppTesters

--- routes/web.php
+++ routes/web.php
@@ -33,86 +33,91 @@ use App\Livewire\Teams\Manage;
 use App\Livewire\Teams\ShowTeam;
 use App\Models\Project;
 use App\Models\Team;
+use CornellCustomDev\LaravelStarterKit\CUAuth\Middleware\AppTesters;
 use CornellCustomDev\LaravelStarterKit\CUAuth\Middleware\CUAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'))->name('welcome');
-Route::get('/help', fn () => view('help'))->name('help');
-Route::get('/updates', fn () => view('updates'))->name('updates');
 
 Route::group(['middleware' => [CUAuth::class]], function () {
     Route::get('login', fn() => redirect()->route('cu-auth.sso-login'))->name('login');
     Route::get('logout', fn() => redirect()->route('cu-auth.sso-logout'))->name('logout');
 });
 
-Route::group(['middleware' => [CUAuth::class]], function () {
-    Route::get('projects/', ViewProjects::class)->name('projects')->can('view-any', Project::class);
-    Route::prefix('project')->name('project.')->group(function () {
-        Route::get('/{project}', ShowProject::class)->name('show')->can('view', 'project');
-        Route::get('/{project}/edit', UpdateProject::class)->name('edit')->can('update', 'project');
-        Route::get('/{project}/scope/create', CreateScope::class)->name('scope.create')->can('update', 'project');
-        Route::get('/{project}/issue/create', CreateProjectIssue::class)->name('issue.create')->can('update', 'project');
-        Route::get('/{project}/report', Report::class)->name('report')->can('view', 'project');
-        Route::get('/{project}/report/raw', function (Project $project) {
-            return view('exports.project-report', [
-                'project' => $project,
-                'issues' => $project->getReportableIssues(),
-                'format' => 'raw',
-            ]);
-        })->name('report.raw')->can('view', 'project');
+Route::group(['middleware' => [AppTesters::class]], function () {
+    Route::get('/help', fn () => view('help'))->name('help');
+    Route::get('/updates', fn () => view('updates'))->name('updates');
+
+    Route::group(['middleware' => [CUAuth::class]], function () {
+        Route::get('projects/', ViewProjects::class)->name('projects')->can('view-any', Project::class);
+        Route::prefix('project')->name('project.')->group(function () {
+            Route::get('/{project}', ShowProject::class)->name('show')->can('view', 'project');
+            Route::get('/{project}/edit', UpdateProject::class)->name('edit')->can('update', 'project');
+            Route::get('/{project}/scope/create', CreateScope::class)->name('scope.create')->can('update', 'project');
+            Route::get('/{project}/issue/create', CreateProjectIssue::class)->name('issue.create')->can('update', 'project');
+            Route::get('/{project}/report', Report::class)->name('report')->can('view', 'project');
+            Route::get('/{project}/report/raw', function (Project $project) {
+                return view('exports.project-report', [
+                    'project' => $project,
+                    'issues' => $project->getReportableIssues(),
+                    'format' => 'raw',
+                ]);
+            })->name('report.raw')->can('view', 'project');
+        });
+
+        Route::prefix('scope/{scope}')->name('scope.')->group(function () {
+            Route::get('', ShowScope::class)->name('show')->can('view', 'scope');
+            Route::get('/edit', UpdateScope::class)->name('edit')->can('update', 'scope');
+            Route::get('/issue/create', CreateIssue::class)->name('issue.create')->can('update', 'scope');
+            Route::get('/issue/siteimprove/create/{rule}/{guideline}', CreateSiteimproveIssue::class)->name('issue.siteimprove.create');
+        });
+
+        Route::prefix('issue/{issue}')->name('issue.')->group(function () {
+            Route::get('', ShowIssue::class)->name('show')->can('view', 'issue');
+            Route::get('/edit', UpdateIssue::class)->name('edit')->can('update', 'issue');
+            Route::get('/item/create', CreateItem::class)->name('item.create')->can('update', 'issue');
+            Route::get('/item/{item}/edit', UpdateItem::class)->name('item.edit')->can('update', 'issue');
+        });
+
+        Route::prefix('teams')->name('teams.')->group(function () {
+            Route::get('', Manage::class)->name('manage')->can('view-any', Team::class);
+            Route::get('{team}', ShowTeam::class)->name('show')->can('view', 'team');
+            Route::get('{team}/project/create', CreateProject::class)->name('project.create')->can('create-projects', 'team');
+        });
+
+        Route::get('chat', Chat::class)->name('chat');
     });
 
-    Route::prefix('scope/{scope}')->name('scope.')->group(function () {
-        Route::get('', ShowScope::class)->name('show')->can('view', 'scope');
-        Route::get('/edit', UpdateScope::class)->name('edit')->can('update', 'scope');
-        Route::get('/issue/create', CreateIssue::class)->name('issue.create')->can('update', 'scope');
-        Route::get('/issue/siteimprove/create/{rule}/{guideline}', CreateSiteimproveIssue::class)->name('issue.siteimprove.create');
+    Route::prefix('guidelines')->name('guidelines.')->group(function () {
+        Route::get('/', ViewGuidelines::class)->name('index');
+        Route::get('/{guideline}', ShowGuideline::class)->name('show');
     });
 
-    Route::prefix('issue/{issue}')->name('issue.')->group(function () {
-        Route::get('', ShowIssue::class)->name('show')->can('view', 'issue');
-        Route::get('/edit', UpdateIssue::class)->name('edit')->can('update', 'issue');
-        Route::get('/item/create', CreateItem::class)->name('item.create')->can('update', 'issue');
-        Route::get('/item/{item}/edit', UpdateItem::class)->name('item.edit')->can('update', 'issue');
+    Route::prefix('criteria')->name('criteria.')->group(function () {
+        Route::get('/', ViewCriteria::class)->name('index');
+        Route::get('/{criterion}', ShowCriterion::class)->name('show');
     });
 
-    Route::prefix('teams')->name('teams.')->group(function () {
-        Route::get('', Manage::class)->name('manage')->can('view-any', Team::class);
-        Route::get('{team}', ShowTeam::class)->name('show')->can('view', 'team');
-        Route::get('{team}/project/create', CreateProject::class)->name('project.create')->can('create-projects', 'team');
+    Route::prefix('categories')->name('categories.')->group(function () {
+        Route::get('/', ViewCategories::class)->name('index');
+        Route::get('/{category}', ShowCategory::class)->name('show');
     });
 
-    Route::get('chat', Chat::class)->name('chat');
-});
+    Route::prefix('act-rules')->name('act-rules.')->group(function () {
+        Route::get('/', ViewRules::class)->name('index');
+        Route::get('/{rule}', ShowRule::class)->name('show');
+    });
 
-Route::prefix('guidelines')->name('guidelines.')->group(function () {
-    Route::get('/', ViewGuidelines::class)->name('index');
-    Route::get('/{guideline}', ShowGuideline::class)->name('show');
-});
+    Route::prefix('siteimprove-rules')->name('siteimprove-rules.')->group(function () {
+        Route::get('/', ViewSiteimproveRules::class)->name('index');
+    });
 
-Route::prefix('criteria')->name('criteria.')->group(function () {
-    Route::get('/', ViewCriteria::class)->name('index');
-    Route::get('/{criterion}', ShowCriterion::class)->name('show');
-});
+    Route::prefix('sia-rules')->name('sia-rules.')->group(function () {
+        Route::get('/', ViewSiaRules::class)->name('index');
+        Route::get('/{rule}', ShowSiaRule::class)->name('show');
+    });
 
-Route::prefix('categories')->name('categories.')->group(function () {
-    Route::get('/', ViewCategories::class)->name('index');
-    Route::get('/{category}', ShowCategory::class)->name('show');
-});
+    Route::get('guidelines.md', Doc::class)->name('guidelines.md');
+    Route::get('analyze', AnalyzePage::class)->name('analyze');
 
-Route::prefix('act-rules')->name('act-rules.')->group(function () {
-    Route::get('/', ViewRules::class)->name('index');
-    Route::get('/{rule}', ShowRule::class)->name('show');
 });
-
-Route::prefix('siteimprove-rules')->name('siteimprove-rules.')->group(function () {
-    Route::get('/', ViewSiteimproveRules::class)->name('index');
-});
-
-Route::prefix('sia-rules')->name('sia-rules.')->group(function () {
-    Route::get('/', ViewSiaRules::class)->name('index');
-    Route::get('/{rule}', ShowSiaRule::class)->name('show');
-});
-
-Route::get('guidelines.md', Doc::class)->name('guidelines.md');
-Route::get('analyze', AnalyzePage::class)->name('analyze');
