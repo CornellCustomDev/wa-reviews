71bed8c50acb7ad37af869d74f6cdc83583e09c8
Eric Woods <woodseowl@gmail.com>
2025-08-25 14:51:13 -0400

    Remove obsolete excel report export

--- app/Livewire/Projects/Report.php
+++ app/Livewire/Projects/Report.php
@@ -2,28 +2,16 @@
 
 namespace App\Livewire\Projects;
 
-use App\Exports\ProjectReportExport;
 use App\Models\Project;
 use App\Models\Scope;
 use App\Services\SiteImprove\SiteimproveService;
-use Illuminate\Support\Str;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
-use Maatwebsite\Excel\Facades\Excel;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class Report extends Component
 {
     public Project $project;
     public ?string $selectedImage = null;
-
-    public function exportReport(): BinaryFileResponse
-    {
-        return Excel::download(
-            export: new ProjectReportExport($this->project),
-            fileName: 'project-report-'.Str::slug($this->project->name).'.xlsx',
-        );
-    }
 
     #[Computed]
     public function issues()
