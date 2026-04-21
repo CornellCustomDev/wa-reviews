479526090b59b7be1180073910a5729719e01998
Eric Woods <woodseowl@gmail.com>
2026-04-21 00:02:45 -0400

    feat: add Comments panel to ScopeSidebar

--- app/Livewire/Scopes/ScopeSidebar.php
+++ app/Livewire/Scopes/ScopeSidebar.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Scopes;
 
 use App\Models\Scope;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 
@@ -16,12 +17,15 @@ class ScopeSidebar extends Component
     public bool $showChat = false;
     #[Url(as: 'analyze', history: true)]
     public bool $showAnalyzer = false;
+    #[Url(as: 'comments', history: true)]
+    public bool $showComments = false;
 
     public function clickContent(): void
     {
         $this->showContent = !$this->showContent;
         $this->showChat = false;
         $this->showAnalyzer = false;
+        $this->showComments = false;
     }
 
     public function clickChat(): void
@@ -29,6 +33,7 @@ class ScopeSidebar extends Component
         $this->showContent = false;
         $this->showChat = !$this->showChat;
         $this->showAnalyzer = false;
+        $this->showComments = false;
     }
 
     public function clickAnalyzer(): void
@@ -36,5 +41,20 @@ class ScopeSidebar extends Component
         $this->showContent = false;
         $this->showChat = false;
         $this->showAnalyzer = !$this->showAnalyzer;
+        $this->showComments = false;
+    }
+
+    public function clickComments(): void
+    {
+        $this->showContent = false;
+        $this->showChat = false;
+        $this->showAnalyzer = false;
+        $this->showComments = !$this->showComments;
+    }
+
+    #[Computed]
+    public function commentsCount(): int
+    {
+        return $this->scope->comments()->count();
     }
 }
